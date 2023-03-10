14548fa26cf53b3abca518fd8fb51a3c4e11105c
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-03-10 12:03:28 +0800

    change return data to array of json object

--- fetchJsonfromPhp/event_calendar.php
+++ fetchJsonfromPhp/event_calendar.php
@@ -3,10 +3,15 @@
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
 if (isset($_POST["noid"])) {
-    $age = array(
-        "receive" => true,
-        "noid" => $_POST["noid"]
-    );
-    echo json_encode($age);
+    $arr = array();
+
+    for ($i = 0; $i < 2; $i++) {
+        array_push($arr, array(
+            "received" => true,
+            "noid" => $_POST["noid"]
+        ));
+    };
+
+    echo json_encode($arr);
     return;
 }
