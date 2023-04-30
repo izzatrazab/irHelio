50f2f9a7da361175576a22e36ad22f5491a77596
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-04-30 22:30:12 +0800

    Update event_calendar.php

--- fetchJsonfromPhp/event_calendar.php
+++ fetchJsonfromPhp/event_calendar.php
@@ -2,7 +2,11 @@
 
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
+
 if (isset($_POST["noid"])) {
+    // fetch any data here (from database/file system/other api endpoint)
+
+    // for example
     $arr = array();
 
     for ($i = 0; $i < 2; $i++) {
@@ -11,7 +15,8 @@ if (isset($_POST["noid"])) {
             "noid" => $_POST["noid"]
         ));
     };
-
+    
     echo json_encode($arr);
+    // end of example
     return;
 }
