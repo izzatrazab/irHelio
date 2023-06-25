0c513324513142a9b25bed1d2d435990a33e471b
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-06-25 18:49:22 +0800

    separate some file and add pico css map file

---
+++ fetchJsonfromPhp/fetchJsonfromPhp.js
@@ -0,0 +1,39 @@
+let clicked = false
+window.onload = () => {
+    hljs.highlightAll()
+}
+
+async function fetchJsonData(number) {
+    console.log(number);
+
+    var formData = new FormData();
+    formData.append('data', number);
+    const data = await fetch("./getJson.php", {
+        method: 'POST',
+        body: formData
+    }).then((res) => res.json())
+
+    document.getElementById("codejson").textContent = JSON.stringify(data, null, "\t")
+    document.getElementById("tbody").replaceChildren()
+    hljs.highlightElement(document.getElementById("codejson"))
+
+    data.forEach(datum => {
+        prependRow(datum.no, datum.receivedData)
+    });
+
+    if (!clicked) {
+        document.getElementById("table").style.display = "block"
+        document.getElementById("json").style.display = "block"
+        clicked = true
+    }
+}
+
+function prependRow(no, receivedData) {
+    let row = document.createElement("tr")
+    let cell_1 = document.createElement("td")
+    cell_1.append(no)
+    let cell_2 = document.createElement("td")
+    cell_2.append(receivedData)
+    row.append(cell_1, cell_2)
+    document.getElementById("tbody").append(row)
+}
--- html2pdf/html2pdf.js
+++ html2pdf/html2pdf.js
@@ -1,3 +1,8 @@
+"use strict";
+window.onload = () => {
+    const style = document.getElementById('article1Style').innerText(document.getElementById('editorArticle1Style')).value = style;
+    console.log(style);
+};
 function printbyID(id, name) {
     var element = document.getElementById(id);
     var opt = {
@@ -16,13 +21,17 @@ function printbyID(id, name) {
         },
         jsPDF: {
             unit: 'cm',
-            format: 'A4',
-            orientation: 'portrait'
+            format: 'a4',
+            orientation: 'landscape'
         }
     };
     //nok save
     // html2pdf().set(opt).from(element).save();
-
     //output saja
-    html2pdf().set(opt).from(element).output('dataurlnewwindow')
+    html2pdf().set(opt).from(element).output('dataurlnewwindow');
+}
+function updateStylebyId(id, editorId) {
+    const editor = document.getElementById(editorId).value;
+    document.getElementById(id).innerText = editor;
+    console.log(editor);
 }
