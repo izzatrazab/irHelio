009347b9e7b534aa42b79317cc07cf018ce616fb
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-07-20 07:17:53 +0800

    use card in homepage

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+//to add new post page, add new item in the $posts array on index 1.
+$posts = [
+    [
+        'href' => 'html-element-to-pdf',
+        'title' => 'Convert HTML element to pdf using html2pdf.js'
+    ],
+    [
+        'href' => 'fetchJsonfromPhp',
+        'title' => 'Fetch Json formatted data from PHP file'
+    ],
+    [
+        'href' => 'bootstraptable',
+        'title' => 'Bootstrap table (directly in php file)'
+    ],
+    [
+        'href' => 'create-table-using-javascript',
+        'title' => 'Create table using javascript'
+    ],
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +29,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./lib/de.css">
     <title>irHelio</title>
+    <style>
+        section {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            gap: 20px;
+            padding-inline: 20px;
+        }
+
+        a {
+            display: contents;
+        }
+
+        .card {
+            padding: 1rem !important;
+            width: 220px;
+            aspect-ratio: 8/10;
+            margin: 0 !important;
+        }
+
+        .card:hover{
+            transform: scale(1.1);
+        }
+    </style>
 </head>
 
 <body>
@@ -16,20 +62,20 @@
             <p>irHelio is a website that is dedicated as a collection of my solutions of the problems that I have encountered. All of the problems are focusing on HTML, Vanilla JS, and PHP</p>
         </hgroup>
         <p>Here are some problems that I already solved (at least it solves my problem). There will be a lot more in the future.</p>
-        <ol>
-            <li>
-                <a href="html-element-to-pdf/">Convert HTML element to pdf using html2pdf.js</a>
-            </li>
-            <li>
-                <a href="fetchJsonfromPhp/">Fetch Json formatted data from PHP file</a>
-            </li>
-            <li>
-                <a href="bootstraptable/">Bootstrap table (directly in php file)</a>
-            </li>
-            <li>
-                <a href="create-table-using-javascript/">Create table using javascript</a>
-            </li>
-        </ol>
+        <section>
+
+            <?php
+            foreach ($posts as $key => $value) {
+                echo '<a href="' . $value['href'] . '/">';
+                echo '<div class="card">';
+                echo '<h2>';
+                echo $value['title'];
+                echo '</h2>';
+                echo '</div>';
+                echo '</a>';
+            }
+            ?>
+        </section>
         <footer>
             <?php
             date_default_timezone_set('Asia/Kuala_Lumpur');
